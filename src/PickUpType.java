7367e218cfb19c4a194ac0955183e75319df145c
Erlibek <shaikemelov99@gmail.com>
2022-03-30 00:33:31 +0600

    inheritance

---
+++ src/PickUpType.java
@@ -0,0 +1,7 @@
+public class PickUpType extends VehicleTypeByBodyTypes {
+
+    @Override
+    public String getTypeName() {
+        return VehicleTypeEnum.PICKUP.name();
+    }
+}
---
+++ src/VehicleTypeByBodyTypes.java
@@ -0,0 +1,15 @@
+public class VehicleTypeByBodyTypes extends VehicleType {
+
+    public VehicleTypeByBodyTypes() {
+        super("Тип автомобиля по кузову");
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        VehicleTypeByBodyTypes that = (VehicleTypeByBodyTypes) object;
+        return attribute != null && attribute.equals(that.attribute);
+    }
+}
